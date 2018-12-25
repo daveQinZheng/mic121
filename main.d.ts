cfd74a73bc3f83786bc1e342d1848189b37c3259
qin <>
2018-12-25 16:29:21 +0800

    no message

---
+++ main.d.ts
@@ -0,0 +1,15 @@
+// Auto-generated. Do not edit.
+
+
+
+    //% color=#E3008C weight=96 icon="\uf012"
+    declare namespace robobloq {
+
+        //% blockId=robobloq_readUntil block="robobloq|read until %delimiter=serial_delimiter_conv"
+        function readUntil(name: string): void;
+    
+     
+    }
+    
+    // Auto-generated. Do not edit. Really.
+    
